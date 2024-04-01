17a5ed8e8e25d19fb945ecb86cb533e03d5330e3
kaizen-design <denis@kaizen-design.ru>
2024-04-01 18:04:56 +0300

    Homepage swiper

--- js/main.js
+++ js/main.js
@@ -2,9 +2,25 @@ const APP = {
   init: () => {
     APP.initSlider(".product-preview-slider");
     APP.initSlider(".product-modal-slider");
+    APP.initMainMenu();
   },
 
-  
+  initMainMenu: () => {
+    const $el = document.querySelector(".menu-slider");
+    if ($el) {
+      new Swiper($el, {
+        slidesPerView: 4,
+        spaceBetween: 48,
+        navigation: {
+            nextEl: `.main-menu .next-btn`,
+            prevEl: `.main-menu .prev-btn`,
+        }
+      });
+      const $menuItems = $el.querySelectorAll('.slider-item');
+      $menuItems[0].focus();
+      $menuItems[0].classList.add('active');
+    }
+  },
 
   initSlider: (selector) => {
     const $el = document.querySelector(selector);
