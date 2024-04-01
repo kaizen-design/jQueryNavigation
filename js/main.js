204cbe21a50b3a863adc6bfb5895da874a3133ca
kaizen-design <denis@kaizen-design.ru>
2024-04-01 19:42:03 +0300

    Homepage navigation completed

--- js/main.js
+++ js/main.js
@@ -6,20 +6,79 @@ const APP = {
   },
 
   initMainMenu: () => {
-    const $el = document.querySelector(".menu-slider");
-    if ($el) {
-      new Swiper($el, {
-        slidesPerView: 4,
-        spaceBetween: 48,
-        navigation: {
-            nextEl: `.main-menu .next-btn`,
-            prevEl: `.main-menu .prev-btn`,
-        }
-      });
-      const $menuItems = $el.querySelectorAll('.slider-item');
-      $menuItems[0].focus();
-      $menuItems[0].classList.add('active');
-    }
+    const $mainMenu = document.querySelector(".menu-slider");
+    
+    if (!$mainMenu) return;
+    
+    const swiper = new Swiper($mainMenu, {
+      slidesPerView: 4,
+      spaceBetween: 48,
+      navigation: {
+          nextEl: `.main-menu .next-btn`,
+          prevEl: `.main-menu .prev-btn`,
+      }
+    });
+    
+    const $menuItems = $mainMenu.querySelectorAll('.slider-item');
+    const menuLength = $menuItems.length;
+    $menuItems[0].focus();
+    $menuItems[0].classList.add('active');
+
+    let activeIndex = 0;
+    let activeSlide = 0;
+
+    $mainMenu.addEventListener('keydown', function(e) {
+      console.log(e);
+      console.log(activeIndex, activeSlide)
+    	switch(e.keyCode){
+        case 37: //LEFT arrow
+          e.preventDefault();
+          console.log('Arrow left click');
+          if ($menuItems[activeIndex - 1]) {
+            activeIndex -= 1;
+            $menuItems.forEach(($el) => $el.classList.remove('active'));
+            $menuItems[activeIndex].classList.add('active');
+            if (activeIndex === activeSlide - 1) {
+              swiper.slidePrev();
+              activeSlide -= 1;
+            }
+          }
+          break;
+        case 38: //UP arrow
+          break;
+        case 39: //RIGHT arrow
+          e.preventDefault();
+          e.stopPropagation();
+          console.log('Arrow right click');
+          if ($menuItems[activeIndex + 1]) {
+            activeIndex += 1;
+            $menuItems.forEach(($el) => $el.classList.remove('active'));
+            $menuItems[activeIndex].classList.add('active');
+            if (activeIndex > activeSlide + 3) {
+              swiper.slideNext();
+              activeSlide += 1;
+            }
+          }
+          break;
+        case 40: //DOWN arrow
+          console.log('Down right click');
+          const $backBtn = document.querySelector('.back-btn');
+          $menuItems.forEach(($el) => $el.classList.remove('active'));
+          $backBtn.focus();
+          break;
+        case 13: //OK button
+          //e.preventDefault();
+          window.location = $menuItems[activeIndex].getAttribute('href');
+          break;
+        case 10009: //RETURN button
+          tizen.application.getCurrentApplication().exit();
+          break;
+        default:
+          console.log('Key code : ' + e.keyCode);
+    		  break;
+    	}
+    });
+    
   },
 
   initSlider: (selector) => {
